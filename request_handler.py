d950a4cea51f58f29d3166fb29d462616dc5c874
turner lynch <turnerjoylynch@gmail.com>
2022-04-16 08:56:31 -0700

    POST method update

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,10 @@
 """Handles requests"""
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_customers, get_single_customer, get_all_employees, get_single_employee
+from views import get_all_animals, get_single_animal, create_animal
+from views import get_all_locations, get_single_location
+from views import get_all_customers, get_single_customer
+from views import get_all_employees, get_single_employee
 
 
 # Here's a class. It inherits from another class.
@@ -62,55 +66,64 @@ class HandleRequests(BaseHTTPRequestHandler):
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
-        def do_GET(self):
-            self._set_headers(200)
-            response = {}  # Default response
+    def do_GET(self):
+        self._set_headers(200)
+        response = {}  # Default response
 
             # Parse the URL and capture the tuple that is returned
-            (resource, id) = self.parse_url(self.path)
+        (resource, id) = self.parse_url(self.path)
 
-            if resource == "animals":
-                if id is not None:
-                    response = f"{get_single_animal(id)}"
+        if resource == "animals":
+            if id is not None:
+                response = f"{get_single_animal(id)}"
 
-                else:
-                    response = f"{get_all_animals()}"
+            else:
+                response = f"{get_all_animals()}"
                     
-            if resource == "locations":
-                if id is not None:
-                    response = f"{get_single_location(id)}"
+        if resource == "locations":
+            if id is not None:
+                response = f"{get_single_location(id)}"
 
-                else:
-                    response = f"{get_all_locations()}"
+            else:
+                response = f"{get_all_locations()}"
                     
-            if resource == "employees":
-                if id is not None:
-                    response = f"{get_single_employee(id)}"
+        if resource == "employees":
+            if id is not None:
+                response = f"{get_single_employee(id)}"
 
-                else:
-                    response = f"{get_all_employees()}"
+            else:
+                response = f"{get_all_employees()}"
                                        
-            if resource == "customers":
-                if id is not None:
-                    response = f"{get_single_customer(id)}"
+        if resource == "customers":
+            if id is not None:
+                response = f"{get_single_customer(id)}"
 
-                else:
-                    response = f"{get_all_customers()}"
+            else:
+                response = f"{get_all_customers()}"
 
-            self.wfile.write(response.encode())
-
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any POST request.
+        self.wfile.write(response.encode())
+        
+    
     def do_POST(self):
-        """Handles POST requests to the server
-        """
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+
+        # Add a new animal to the list. 
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_animal}".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
